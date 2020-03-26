44f46d46c7577c53a71e64a894aca13795874577
Aditya Gupta <aditya@aditya.local>
2020-03-25 22:08:30 -0500

    baseline done

--- mp4/viterbi.py
+++ mp4/viterbi.py
@@ -3,6 +3,8 @@ This is the main entry point for MP4. You should only modify code
 within this file -- the unrevised staff files will be used for all other
 files and classes when code is run, so be careful to not modify anything else.
 """
+import numpy as np
+from collections import Counter
 
 def baseline(train, test):
     '''
@@ -15,7 +17,30 @@ def baseline(train, test):
             E.g. [[(word1, tag1), (word2, tag2)...], [(word1, tag1), (word2, tag2)...]...]
     '''
     predicts = []
-    raise Exception("You must implement me")
+    word_tag = {}
+    tag_ct = Counter()
+    for sentence in train:
+        for w_tag in sentence:
+            w, t = w_tag
+
+            if w not in word_tag:
+                word_tag[w] = Counter()
+            word_tag[w][t] += 1
+
+            tag_ct[t] += 1
+
+    t_max = max(tag_ct.keys(), key=(lambda key: tag_ct[key]))
+
+    for sentence in test:
+        tag_pred = []
+        for word in sentence:
+            if word in word_tag:
+                max_t = max((word_tag[word]).keys(), key=lambda key:word_tag[word][key])
+                tag_pred.append((word, max_t))
+            else:
+                tag_pred.append((word, t_max))
+        predicts.append(tag_pred)
+
     return predicts
 
 
