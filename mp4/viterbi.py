6cf1ef6cbf07bbca79270763198755ac561cf6dc
Aditya Gupta <gupta.adityav@gmail.com>
2020-03-27 18:59:39 -0500

    one test for part2 has accuracy .932 != .94

--- mp4/viterbi.py
+++ mp4/viterbi.py
@@ -87,7 +87,7 @@ def make_b_ptr(sentence, tags):
         back_ptr.append({tag:None for tag in list(tags)})
     return back_ptr
 
-def helper(sentence,matrix, back_ptr, initial_prob, ip_uk, transition_prob, tp_uk, emission_prob, ep_uk):
+def helper(sentence,matrix, back_ptr, initial_prob, ip_uk, transition_prob, tp_uk, emission_prob, ep_uk, isDict):
     for key, value in matrix[0].items():
         pi = 0
         b = 0
@@ -98,7 +98,10 @@ def helper(sentence,matrix, back_ptr, initial_prob, ip_uk, transition_prob, tp_u
         if (sentence[0], key) in emission_prob:
             b = emission_prob[(sentence[0], key)]
         else:
-            b = ep_uk
+            if isDict == 0:
+                b = ep_uk
+            else:
+                b = ep_uk[key]
         matrix[0][key] = pi+b
     for i in range(1, len(matrix)):
         for k in matrix[i].keys():
@@ -108,7 +111,10 @@ def helper(sentence,matrix, back_ptr, initial_prob, ip_uk, transition_prob, tp_u
             if (sentence[i], k) in emission_prob:
                 b = emission_prob[(sentence[i], k)]
             else:
-                b= ep_uk
+                if isDict == 0:
+                    b= ep_uk
+                else:
+                    b = ep_uk[key]
             for k_prime in matrix[i-1].keys():
                 a = 0
                 if (k_prime, k) in transition_prob:
@@ -205,21 +211,41 @@ def viterbi_p1(train, test):
     for s in test:
         matrix = make_matrix(s, tags)
         back_ptr = make_b_ptr(s, tags)
-        estimated_test[i] = helper(s,matrix, back_ptr, initial_prob, ip_uk, transition_prob, tp_uk, emission_prob, ep_uk)
+        estimated_test[i] = helper(s,matrix, back_ptr, initial_prob, ip_uk, transition_prob, tp_uk, emission_prob, ep_uk, 0)
         i += 1
 
     predicts = []
     predicts = estimated_test
     return predicts
 
-    # build the trellis
+# part 2 hapax
+def hapax(train, tags, alpha):
+    
+    wt_ctr = Counter()
+    for s in train:
+        for pair in s:
+            wt_ctr[pair[0]] += 1
+    wt_ctr = dict(wt_ctr)
+    happax = [k for k,v in wt_ctr.items() if v==1]
+    happax = set(happax)
 
-    #estimated_test = [[] for i in range(len(test))]
-    #for s in test:
-    #    mat = matrix(s, tags)
-    #    back_ptr = make_b_ptr(s, tags)
-    #    estimated_test[i] = viterbi_helper(s, mat, back_ptr, init_prob, ip_uk, trans_prob, trans_prob_uk, emission_prob, emission_prob_uk)
-    #predicts = estimated_test
+    hap_tags = Counter()
+    for s in train:
+        for pair in s:
+            if pair[0] in happax:
+                hap_tags[pair[1]] += 1
+    hap_tags = dict(hap_tags)
+    sum_ = sum(hap_tags.values())
+   
+    #hap_tags = {tag:alpha/(sum_+len(tags)*alpha) for tag in tags if tag not in hap_tags}
+    for tag in tags:
+        if tag not in hap_tags:
+            hap_tags[tag] = alpha/(sum_+len(tags)*alpha)
+    #hap_tags = {k: (v+alpha)/(sum_ + alpha*len(tags)) for k,v in }
+    for k,v in hap_tags.items():
+        hap_tags[k] = (v+alpha)/(sum_ + alpha*len(tags))
+    return hap_tags
+
 
 def viterbi_p2(train, test):
     '''
@@ -231,8 +257,81 @@ def viterbi_p2(train, test):
     output: list of sentences with tags on the words
             E.g. [[(word1, tag1), (word2, tag2)...], [(word1, tag1), (word2, tag2)...]...]
     '''
+    k = 10**(-5)
 
+    # make the sets of unique words and tags
+    tot_pairs = 0
+    words = set()
+    tags = set()
+    for s in train:
+        for pair in s:
+            tot_pairs += 1
+            w,t = pair
+            words.add(w)
+            tags.add(t)
 
+    # initial prob
+    init_tag_ct = Counter()
+    for sentence in train:
+        init_tag_ct[sentence[0][1]] += 1
+    initial_prob = dict(init_tag_ct)
+
+    for (t, c) in initial_prob.items():
+        initial_prob[t] = log((c+k)/(len(train) + k*len(tags)))
+    ip_uk = log(k/(len(train) + k*len(tags)))
+
+    # transition prob
+    transition = []
+    for s in train:
+        for i in range(1, len(s)):
+            curr_t = s[i][1]
+            prev_t = s[i-1][1]
+            transition.append((prev_t, curr_t))
+    transition_prob = dict(Counter(transition))
+    #tp_uk = dict()
+    for tag_1 in list(tags):
+        denominator = 0
+        for tag in list(tags):
+            if (tag_1, tag) in transition_prob:
+                denominator += transition_prob[(tag_1, tag)]
+        for tag in list(tags):
+            if (tag_1, tag) in transition_prob:
+                transition_prob[(tag_1, tag)] = log((transition_prob[(tag_1,tag)]+k)/(denominator+k*(len(tags)+1)))
+            else:
+                transition_prob[(tag_1, tag)] = log(k/(tot_pairs+k*(len(tags)+1))) #might need to change the len used in denominator
+    tp_uk = log(k/(tot_pairs+k*(len(tags)+1)))
+
+    # hapax calculations
+    hapax_dict = hapax(train, tags, k)
+
+    # emission prob
+    emission_prob = Counter()
+    tag_ct = Counter()
+    for s in train:
+        for wt_pair in s:
+            emission_prob[wt_pair] += 1
+            tag_ct[wt_pair[1]] += 1
+    emission_prob = dict(emission_prob)
+
+    for tag in list(tags):
+        denominator = 0
+        for word in list(words):
+            if (word, tag) in emission_prob:
+                denominator += emission_prob[(word,tag)]
+        for word in list(words):
+            if (word, tag) in emission_prob:
+                emission_prob[(word, tag)]=log((emission_prob[(word,tag)]+k*hapax_dict[tag])/(denominator+k*(len(words)+1)*hapax_dict[tag]))
+            else:
+                emission_prob[(word, tag)] = log((k*hapax_dict[tag])/(tag_ct[tag]+k*hapax_dict[tag]*(len(words)+1)))
+    ep_uk = {tag:log((k*hapax_dict[tag])/(tag_ct[tag]+k*hapax_dict[tag]*(len(words)+1))) for tag in list(tags)}
+
+    estimated_test = [[] for i in range(len(test))]
+    i= 0
+    for s in test:
+        matrix = make_matrix(s, tags)
+        back_ptr = make_b_ptr(s, tags)
+        estimated_test[i] = helper(s, matrix, back_ptr, initial_prob, ip_uk, transition_prob, tp_uk, emission_prob, ep_uk, 1)
+        i += 1
     predicts = []
-    raise Exception("You must implement me")
+    predicts = estimated_test
     return predicts
